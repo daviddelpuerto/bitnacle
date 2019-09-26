c5027365c0fccd086be59258fc59e22725d965f4
David del Puerto <ddelpuertomartin@gmail.com>
2019-09-27 00:23:32 +0200

    Created a genericLogger to test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
 const { expect } = require('chai');
 const Bitnacle = require('../index');
+const genericLogger = new Bitnacle();
 
 describe('#constructor', function() {
     it('should create a new instance of Bitnacle with default simple format', function() {
-        const logger = new Bitnacle();
-        expect(logger).to.be.an.instanceOf(Bitnacle);
-        expect(logger.format).to.equal('simple');
+        expect(genericLogger).to.be.an.instanceOf(Bitnacle);
+        expect(genericLogger.format).to.equal('simple');
     });
 
     it('should create json logger if json format is specified', function() {
@@ -21,19 +21,17 @@ describe('#constructor', function() {
 });
 
 describe('#getRequestFromExtraInfo()', function() {
-    const logger = new Bitnacle();
-
     it('should return an empty object if extraInfo or extraInfo.req are missing', function() {
-        expect(logger.getRequestPropsFromExtraInfo()).to.be.an.instanceOf(Object).to.be.empty;
-        expect(logger.getRequestPropsFromExtraInfo({})).to.be.an.instanceOf(Object).to.be.empty;
+        expect(genericLogger.getRequestPropsFromExtraInfo()).to.be.an.instanceOf(Object).to.be.empty;
+        expect(genericLogger.getRequestPropsFromExtraInfo({})).to.be.an.instanceOf(Object).to.be.empty;
     });
 
     it('should throw if extraInfo.req is not an object', function() {
-        expect(() => logger.getRequestPropsFromExtraInfo({ req: 'req' })).to.throw();
+        expect(() => genericLogger.getRequestPropsFromExtraInfo({ req: 'req' })).to.throw();
     });
 
     it('should return an object with request info', function() {
-        expect(logger.getRequestPropsFromExtraInfo({
+        expect(genericLogger.getRequestPropsFromExtraInfo({
             req: { method: 'GET' }
         })).to.be.an.instanceOf(Object).to.have.property('method', 'GET');
     });
