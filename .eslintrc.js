4c25e6d8364be7b254356ecc74e7087215739b32
David del Puerto <ddelpuertomartin@gmail.com>
2019-09-26 14:57:44 +0200

    Added eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion:8,
+        sourceType: 'module',
+    },
+    rules: {
+        semi: 'error',
+        indent: ['error', 4],
+        quotes: ['error', 'single'],
+        eqeqeq: 'error',
+        'linebreak-style': ['error', 'unix']
+    },
+    env: {
+        node: true,
+        es6: true
+    }
+};
