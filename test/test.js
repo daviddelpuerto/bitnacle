530a9b4698bafdafb3f967f1bb2bd0094c216f15
David del Puerto <ddelpuertomartin@gmail.com>
2019-09-27 17:56:10 +0200

    Added test for 100% coverage

--- test/test.js
+++ test/test.js
@@ -1,6 +1,12 @@
 const { expect } = require('chai');
+const { stdout, stderr } = require('test-console');
 const Bitnacle = require('../index');
 const genericLogger = new Bitnacle();
+
+function testConsoleOutput({ output, level, message }) {
+    const regEx = new RegExp(`\\[\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}:\\d{3}\\+\\d{4}\\] \\[${level}\\] \\[${message}\\]\\n`);
+    output[0].match(regEx);
+};
 
 describe('#constructor', function() {
     it('should create a new instance of Bitnacle with default simple format', function() {
@@ -22,8 +28,13 @@ describe('#constructor', function() {
 
 describe('#getRequestFromExtraInfo()', function() {
     it('should return an empty object if extraInfo or extraInfo.req are missing', function() {
-        expect(genericLogger.getRequestPropsFromExtraInfo()).to.be.an.instanceOf(Object).to.be.empty;
-        expect(genericLogger.getRequestPropsFromExtraInfo({})).to.be.an.instanceOf(Object).to.be.empty;
+        expect(genericLogger.getRequestPropsFromExtraInfo())
+            .to.be.an.instanceOf(Object)
+            .to.be.empty;
+            
+        expect(genericLogger.getRequestPropsFromExtraInfo({}))
+            .to.be.an.instanceOf(Object)
+            .to.be.empty;
     });
 
     it('should throw if extraInfo.req is not an object', function() {
@@ -36,5 +47,114 @@ describe('#getRequestFromExtraInfo()', function() {
         }))
         .to.be.an.instanceOf(Object)
         .to.have.property('method', 'GET');
+    });
+});
+
+describe('#log()', function() {
+    it(`should throw if extra info is passed and it's not an object`, function() {
+        expect(() => genericLogger.log({
+            level: 'LEVEL',
+            message: 'Test message',
+            extraInfo: 'extraInfo'
+        })).to.throw();
+    });
+
+    it('should log extraInfo.extra', function() {
+        const level = 'LEVEL';
+        const message = 'Test message'
+        const extra1 = 'extra1';
+        const extra2 = 'extra2';
+
+        const output = stdout.inspectSync(function() {
+            genericLogger.log({
+                level,
+                message,
+                extraInfo: {
+                    extra: {
+                        extra1,
+                        extra2
+                    }
+                }
+            });
+        });
+
+        expect(output[0])
+            .to.include(extra1)
+            .to.include(extra2);
+
+        testConsoleOutput({
+            level,
+            message,
+            output
+        });
+    });
+
+});
+
+describe('#error()', function() {
+    it('Bitnacle.error should log correct message to process.stderr', function() {
+        const level = 'ERROR';
+        const message = 'Error message';
+
+        const output = stderr.inspectSync(function() {
+            genericLogger[level.toLowerCase()](message);
+        });
+
+        testConsoleOutput({
+            output,
+            level, 
+            message
+        });
+    });
+});
+
+describe('#warning()', function() {
+    it('Bitnacle.warning should log correct message to process.stderr', function() {
+        const level = 'WARNING';
+        const message = 'Warning message';
+
+        const output = stderr.inspectSync(function() {
+            genericLogger[level.toLowerCase()](message);
+        });
+
+        testConsoleOutput({
+            output,
+            level, 
+            message
+        });
+    });
+});
+
+describe('#info()', function() {
+    it('Bitnacle.info should log correct message to process.stdout', function() {
+        const level = 'INFO';
+        const message = 'Info message';
+
+        const output = stdout.inspectSync(function() {
+            genericLogger[level.toLowerCase()](message);
+        });
+
+        testConsoleOutput({
+            output,
+            level, 
+            message
+        });
+    });
+});
+
+describe('#debug()', function() {
+    it('Bitnacle.debug should log correct message to process.stdout', function() {
+        const level = 'DEBUG';
+        const message = 'Debug message';
+
+        const output = stdout.inspectSync(function() {
+            genericLogger[level.toLowerCase()](message);
+        });
+
+        testConsoleOutput({
+            output,
+            level, 
+            message
+        });     
     });
 });
