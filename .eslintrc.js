854ae5275384a4977b46bf03c9d15e049c6cc6e2
David del Puerto <ddelpuertomartin@gmail.com>
2019-10-25 20:31:20 +0200

    Using airBnB code style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,20 @@
 module.exports = {
-    extends: 'eslint:recommended',
-    parserOptions: {
-        ecmaVersion: 8,
-        sourceType: 'module',
-    },
-    rules: {
-        semi: 'error',
-        indent: ['error', 4],
-        quotes: ['error', 'single'],
-        eqeqeq: 'error',
-        'linebreak-style': ['error', 'unix']
-    },
-    env: {
-        node: true,
-        es6: true
-    }
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'class-methods-use-this': 'off'
+  },
 };
