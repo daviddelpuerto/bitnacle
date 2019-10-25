606f76f82737dc22cf2755f414f9c607ab09ee28
David del Puerto <ddelpuertomartin@gmail.com>
2019-10-25 20:31:49 +0200

    Fixed objects not being logged correctly

--- index.js
+++ index.js
@@ -3,98 +3,100 @@
 const { bitnacleLevels, bitnacleFormats, bitnacleTimer } = require('bitnacle-helpers');
 
 class Bitnacle {
+  constructor(options) {
+    this.options = typeof options === 'object' ? options : {};
+    this.format = 'simple';
 
-    constructor (options) {
-        this.options = typeof options === 'object' ? options : {};
-        this.format = 'simple';
+    if (this.options.format) {
+      if (this.options.format === 'extended' || bitnacleFormats[this.options.format] === undefined) {
+        throw new Error('Invalid format for Bitnacle, use one of the following: simple or json.');
+      }
 
-        if (this.options.format) {
-            if (this.options.format === 'extended' || bitnacleFormats[this.options.format] === undefined) {
-                throw new Error('Invalid format for Bitnacle, use one of the following: simple or json.');
-            }
+      this.format = this.options.format;
+    }
+  }
 
-            this.format = this.options.format;
-        }
+  getRequestPropsFromExtraInfo(extraInfo) {
+    if (!extraInfo || !extraInfo.req) return {};
 
+    const { req } = extraInfo;
+
+    if (typeof req !== 'object') {
+      throw new Error('extraInfo.req must be an object');
     }
 
-    getRequestPropsFromExtraInfo(extraInfo) {
-        if (!extraInfo || !extraInfo.req) return {};
+    const method = req && req.method;
+    const endpoint = req && (req.originalUrl || req.url);
+    const remoteAddress = req && (req.clientIp || req.ip);
+    const id = req && req.id;
 
-        const { req } = extraInfo;
+    return {
+      method, endpoint, remoteAddress, id,
+    };
+  }
 
-        if (typeof req !== 'object') {
-            throw new Error('extraInfo.req must be an object');
-        }
-
-        const method = req && req.method;
-        const endpoint = req && (req.originalUrl || req.url);
-        const remoteAddress = req && (req.clientIp || req.ip);
-        const id = req && req.id;
-
-        return { method, endpoint, remoteAddress, id };
+  log({ level, message, extraInfo }) {
+    if (extraInfo && typeof extraInfo !== 'object') {
+      throw new Error('You must pass an object as "extraInfo" to Bitnacle');
     }
 
-    log({ level, message, extraInfo }) {
 
-        if (extraInfo && typeof extraInfo !== 'object') {
-            throw new Error('You must pass an object as "extraInfo" to Bitnacle');
-        }
+    const messageToLog = message.message || message;
+    // console.log(messageToLog);
 
-        const time = bitnacleTimer.getRequestTime();
+    const time = bitnacleTimer.getRequestTime();
 
-        const req = this.getRequestPropsFromExtraInfo(extraInfo);
-        const extra = extraInfo && extraInfo.extra;
+    const req = this.getRequestPropsFromExtraInfo(extraInfo);
+    const extra = extraInfo && extraInfo.extra;
 
-        const logMessageObject = {
-            time,
-            level,
-            req,
-            extra,
-            message
-        };
+    const logMessageObject = {
+      time,
+      level,
+      req,
+      extra,
+      message: messageToLog,
+    };
 
-        const logMessage = bitnacleFormats[this.format](logMessageObject);
+    const logMessage = bitnacleFormats[this.format](logMessageObject);
 
-        if (level && (level === bitnacleLevels.levels.ERROR || level === bitnacleLevels.levels.WARNING)) {
-            process.stderr.write(`${logMessage}\n`);
-        } else {
-            process.stdout.write(`${logMessage}\n`);
-        }
+    if (level && (level === bitnacleLevels.levels.ERROR)) {
+      process.stderr.write(`${logMessage}\n`);
+    } else {
+      process.stdout.write(`${logMessage}\n`);
     }
+  }
 
-    error(err, extraInfo) {
-        this.log({
-            level: bitnacleLevels.levels.ERROR, 
-            message: err, 
-            extraInfo
-        });
-    }
+  error(err, extraInfo) {
+    this.log({
+      level: bitnacleLevels.levels.ERROR,
+      message: err,
+      extraInfo,
+    });
+  }
 
-    warning(message, extraInfo) {
-        this.log({
-            level: bitnacleLevels.levels.WARNING, 
-            message, 
-            extraInfo
-        });
-    }
+  warning(message, extraInfo) {
+    this.log({
+      level: bitnacleLevels.levels.WARNING,
+      message,
+      extraInfo,
+    });
+  }
 
-    info(message, extraInfo) {
-        this.log({
-            level: bitnacleLevels.levels.INFO, 
-            message, 
-            extraInfo
-        });
-    }
+  info(message, extraInfo) {
+    this.log({
+      level: bitnacleLevels.levels.INFO,
+      message,
+      extraInfo,
+    });
+  }
 
-    debug(message, extraInfo) {
-        this.log({
-            level: bitnacleLevels.levels.DEBUG, 
-            message, 
-            extraInfo
-        });
-    }
-
+  debug(message, extraInfo) {
+    this.log({
+      level: bitnacleLevels.levels.DEBUG,
+      message,
+      extraInfo,
+    });
+  }
 }
 
 module.exports = Bitnacle;
